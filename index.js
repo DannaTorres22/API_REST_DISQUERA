a407ddfc53358615c0cb0b75894c0c5a31c5183e
DannaTorres22 <104436270+DannaTorres22@users.noreply.github.com>
2022-11-22 14:00:16 -0500

    a

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose= require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const routes =require('./routes')
+
+const app = express();
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect(
+    "mongodb://localhost/Disquera",
+    {
+        useNewUrlParser:true,
+    }
+);
+//habilitar body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+//habilitar cors
+app.use(cors());
+
+
+app.use('/', routes());
+
+app.listen(5000,function() {
+    console.log('servidor web de express en ejecucion')
+})
